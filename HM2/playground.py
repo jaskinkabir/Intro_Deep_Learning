875594cdc5546cd1b8f42019d8c875d0630beefb
jaskinkabir <jaskinkabir@gmail.com>
2025-02-17 20:58:24 -0500

    Fixed plotting issues hopefully, wrote initial alexnet code

--- HM2/playground.py
+++
@@ -1,3 +0,0 @@
-
-from jlib.cifar_preprocessing import get_cifar_loaders, delete_deletables
-from jlib.alexnet import AlexNet, ConvParams
--- HM2/run_models.sh
+++
@@ -1,8 +0,0 @@
-# loop over every python file in model_scripts, echo the name of the file and a seperator line, then run the file
-source ../env/bin/activate
-for file in model_scripts/*.py
-do
-    echo $file
-    echo "-----------------------------------------"
-    python3.11 $file
-done
