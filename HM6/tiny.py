d49051a927babeda4dbce17a9515fe2bda0a9ff6
jaskinkabir <jaskinkabir@gmail.com>
2025-04-15 07:14:38 -0400

    added run_swin

--- HM6/tiny.py
+++ HM6/tiny.py
@@ -1,5 +1,5 @@
 from jlib.swin import Swin
-from run_model import train_model
+from run_swin import train_model
 device = 'cuda:0'
 
 model = Swin(model_name='microsoft/swin-tiny-patch4-window7-224', num_classes=100, device=device)
